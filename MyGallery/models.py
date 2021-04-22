d2e4e439ff5830ec1d24e26b3c12e9a803d82fda
Charity Mutoni <charitytoni0@gmail.com>
2021-04-22 09:51:44 +0200

    made changes in models.py

--- MyGallery/models.py
+++ MyGallery/models.py
@@ -53,7 +53,7 @@ class Photo(models.Model):
     name = models.CharField(max_length=244)
     description = models.TextField()
     location = models.ForeignKey(Location)
-    categories = models.ForeignKey(Category)
+    category = models.ForeignKey(Category)
     post_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to="images/")
 
