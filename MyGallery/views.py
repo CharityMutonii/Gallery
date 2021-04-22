900d84119f7c48c943adea0fe50f02bb9204b8f4
Charity Mutoni <charitytoni0@gmail.com>
2021-04-22 07:09:40 +0200

    created view function to display all photos

--- MyGallery/views.py
+++ MyGallery/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Photo, Location,Category
 # Create your views here.
+
+def index(request):
+    photos = Photo.all_photos()
+    return render(request, "index.html", {'photos':photos})
