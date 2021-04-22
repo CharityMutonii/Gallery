1a17ec59ce24e9c7851a01c62e8bf703168b4f46
Charity Mutoni <charitytoni0@gmail.com>
2021-04-22 23:52:49 +0200

    created a view function search_results

--- MyGallery/views.py
+++ MyGallery/views.py
@@ -6,4 +6,17 @@ def index(request):
     photos = Photo.all_photos()
     return render(request, "index.html", {'photos':photos})
 
+  
+def search_results(request):
+    if 'photo' in request.GET and request.GET["photo"]:
+        search_term = request.GET.get("photo")
+        searched_photos = Image.search_by_name(search_term)
+        print("searched_photos")
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"photos": searched_photos})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})      
 
