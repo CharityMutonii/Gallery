ff8a98d868b4dbfa9122b41126046cdec9dd551e
Charity Mutoni <charitytoni0@gmail.com>
2021-04-22 12:30:09 +0200

    added models to the admin page

--- MyGallery/admin.py
+++ MyGallery/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Photo, Location, Category
+
+class PhotoAdmin(admin.ModelAdmin):
+    filter_horizontal =('category',)
 
 # Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Photo,PhotoAdmin)
+admin.site.register(Category)
