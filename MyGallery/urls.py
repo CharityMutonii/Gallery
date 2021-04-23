07b1cff5ae908352f162fa67e1421bbb9941bd07
Charity Mutoni <charitytoni0@gmail.com>
2021-04-23 02:10:14 +0200

    created url to access the view function display_location

--- MyGallery/urls.py
+++ MyGallery/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$', views.index, name='index'),
     url(r'^search/', views.search_results, name='search_results'),
+     url(r'^location/(?P<location>\D+)/$',views.display_location,name ='location'),
 
 ]
 if settings.DEBUG:
